3c62d71df4150056eb829e39256b8352470523b2
Iktiar Nazib <iktiarnazib@gmail.com>
2026-04-22 11:34:29 +0600

    Completed Authentication UI

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:authenticationapp/components/my_sign_in_button.dart';
 import 'package:authenticationapp/components/my_text_field.dart';
+import 'package:authenticationapp/components/square_tile.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -17,91 +18,121 @@ class LoginPage extends StatelessWidget {
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              //Size
-              const SizedBox(height: 50),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //Size
+                const SizedBox(height: 50),
 
-              //Logo
-              const Icon(Icons.lock, size: 100),
+                //Logo
+                const Icon(Icons.lock, size: 100),
 
-              //space between
-              const SizedBox(height: 50),
-              //Welcome Back, You've been missed!
-              Text(
-                "Welcome Back, You've been missed!",
-                style: TextStyle(color: Colors.grey[700], fontSize: 16),
-              ),
+                //space between
+                const SizedBox(height: 50),
+                //Welcome Back, You've been missed!
+                Text(
+                  "Welcome Back, You've been missed!",
+                  style: TextStyle(color: Colors.grey[700], fontSize: 16),
+                ),
 
-              //space between
-              const SizedBox(height: 25),
+                //space between
+                const SizedBox(height: 25),
 
-              //username textfield
-              MyTextField(
-                controller: emailController,
-                hint: hintUser,
-                obscureText: false,
-              ),
+                //username textfield
+                MyTextField(
+                  controller: emailController,
+                  hint: hintUser,
+                  obscureText: false,
+                ),
 
-              //space between
-              const SizedBox(height: 10),
+                //space between
+                const SizedBox(height: 10),
 
-              //password textfield
-              MyTextField(
-                controller: passController,
-                hint: hintPass,
-                obscureText: true,
-              ),
-              const SizedBox(height: 10),
-              //forgot password?
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 25.0),
-                    child: Text('Forgot Password?'),
-                  ),
-                ],
-              ),
-
-              //space between
-              const SizedBox(height: 25),
-
-              //sign in button
-              MySignInButton(onTap: onSignInUser),
-
-              //Space Between
-              SizedBox(height: 50),
-
-              //or continue with
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
+                //password textfield
+                MyTextField(
+                  controller: passController,
+                  hint: hintPass,
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10),
+                //forgot password?
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Expanded(
-                      child: Divider(thickness: 0.5, color: Colors.grey[400]),
-                    ),
                     Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Or Continue With',
-                        style: TextStyle(color: Colors.grey[700]),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(thickness: 0.5, color: Colors.grey[400]),
+                      padding: const EdgeInsets.only(right: 25.0),
+                      child: Text('Forgot Password?'),
                     ),
                   ],
                 ),
-              ),
 
-              //google, and apple signing button
-              Row(children: [
-                
-              ],),
+                //space between
+                const SizedBox(height: 25),
 
-              //not a member, register now
-            ],
+                //sign in button
+                MySignInButton(onTap: onSignInUser),
+
+                //Space Between
+                SizedBox(height: 50),
+
+                //or continue with
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(thickness: 0.5, color: Colors.grey[400]),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Or Continue With',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(thickness: 0.5, color: Colors.grey[400]),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 50),
+
+                //google, and apple signing button
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //google button
+                    SquareTile(imagePath: 'lib/images/googlelogo.png'),
+                    SizedBox(width: 25),
+                    //apple button
+                    SquareTile(imagePath: 'lib/images/applelogo.png'),
+                  ],
+                ),
+
+                //space between
+                SizedBox(height: 50),
+                //not a member, register now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Not a member?',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                    SizedBox(width: 4),
+                    Text(
+                      'Register now',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
