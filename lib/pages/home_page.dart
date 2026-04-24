4edd2403a71eb3c5f7a16a2cc0d82ef25b04851a
Iktiar Nazib <iktiarnazib@gmail.com>
2026-04-24 22:16:59 +0600

    Usename update implementation in the app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,18 @@
+import 'package:authenticationapp/pages/update_username.dart';
+import 'package:authenticationapp/services/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //update username controller
+  TextEditingController updateUsernameTextControllder = TextEditingController();
 
   //current user
   final user = FirebaseAuth.instance.currentUser!;
@@ -10,6 +20,12 @@ class HomePage extends StatelessWidget {
   //logout
   void onLogOutPressed() {
     FirebaseAuth.instance.signOut();
+  }
+
+  //update username
+  onUpdateUsernamePressed(String newUserName) async {
+    await AuthService().updateUserName(newUserName);
+    setState(() {});
   }
 
   @override
@@ -24,7 +40,42 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(child: Text('Logging In: ${user.email!}')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.black,
+              backgroundImage: NetworkImage(
+                '${FirebaseAuth.instance.currentUser!.photoURL ?? Icon(Icons.account_circle, color: Colors.purple)}',
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Logging In: ${FirebaseAuth.instance.currentUser!.displayName ?? 'User'}',
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+            ),
+            Text("${FirebaseAuth.instance.currentUser!.email}"),
+            //space between
+            SizedBox(height: 10),
+            TextButton(
+              onPressed: () async {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return UpdateUsername();
+                    },
+                  ),
+                );
+                setState(() {});
+              },
+              child: Text('Update username'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
